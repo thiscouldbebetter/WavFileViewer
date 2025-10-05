13fb63bec97b52437057484ac752b24467af7070
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2025-10-05 00:30:59 -0400

    Added _Imports.ts for downstream projects to use.

---
+++ Source/_Imports.js
@@ -0,0 +1,5 @@
+"use strict";
+var wfv = ThisCouldBeBetter.WavFileViewer;
+var SoundFromWavFile = wfv.SoundFromWavFile;
+var WavFile = wfv.WavFile;
+var WavFileSamplingInfo = wfv.WavFileSamplingInfo;
---
+++ Source/_Imports.ts
@@ -0,0 +1,7 @@
+
+import wfv = ThisCouldBeBetter.WavFileViewer;
+
+import SoundFromWavFile = wfv.SoundFromWavFile;
+import WavFile = wfv.WavFile;
+import WavFileSamplingInfo = wfv.WavFileSamplingInfo;
+
